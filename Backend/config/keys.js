286eb8fb97a04a0495aa1e59f62e22a5de172c80
Naik, Kiran Gurunath <kinaik@iu.edu>
2023-03-06 12:04:43 -0500

    Backend added
    Register
    Login
    ManagerLogin

---
+++ Backend/config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI: "mongodb+srv://postgres:postgres@cluster0.iqvygrf.mongodb.net/?retryWrites=true&w=majority",
+    secretOrKey: "secret",
+    frontend_google_clientID:"973282407747-5e4l7ut9st7c6aqace5d1avdjcjp8o3s.apps.googleusercontent.com"
+  };
