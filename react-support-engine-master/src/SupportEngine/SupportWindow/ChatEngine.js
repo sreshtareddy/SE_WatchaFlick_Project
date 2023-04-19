944ba843b328e74675ff5d3a8929d50a261c4645
sreshtareddy <sreshtareddy001@gmail.com>
2023-04-18 21:45:37 -0400

    sample

---
+++ react-support-engine-master/src/SupportEngine/SupportWindow/ChatEngine.js
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from "react";
+
+import { ChatEngineWrapper, Socket, ChatFeed } from 'react-chat-engine'
+
+const ChatEngine = props => {
+    const [showChat, setShowChat] = useState(false)
+    const projectID="90375715-744b-443f-a831-22c13a091994";
+
+    useEffect(() => {
+        if (props.visible) {
+            setTimeout(() => {
+                setShowChat(true)
+            }, 500)
+        }
+    })
+
+    return (
+        <div
+            className='transition-5'
+            style={{
+                ...styles.chatEngineWindow,
+                ...{ 
+                    height: props.visible ? '100%' : '0px',
+                    zIndex: props.visible ? '100' : '0',
+                }
+            }}
+        >
+            {
+                showChat &&
+                <ChatEngineWrapper>
+                    <Socket 
+                        projectID={projectID}
+                        userName={props.user.email}
+                        userSecret={props.user.email}
+                    />
+                    <ChatFeed activeChat={props.chat.id} />
+                </ChatEngineWrapper>
+            }
+        </div>
+    )
+}
+
+export default ChatEngine;
+
+const styles = {
+    chatEngineWindow: {
+        width: '100%',  
+        backgroundColor: '#fff',
+    }
+}
